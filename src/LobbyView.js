82412bbc32d8aca2afda4008ac0c9bd5224810b4
Ryan Cavasso <cavasso.ryan@gmail.com>
2021-03-13 22:13:08 -0600

    Added more functionality, it now displays menu item names and prices which can be changed.

--- src/LobbyView.js
+++ src/LobbyView.js
@@ -1,10 +1,21 @@
 import React, {useState} from 'react';
 import './LobbyView.css';
 
-const tables = [
+/*const tables = [
     "Table 1",  "Table 2",  "Table 3",  "Table 4",  "Table 5",  "Table 6",  "Table 7",  "Table 8",  "Table 9",
     "Table 10", "Table 11", "Table 12", "Table 13", "Table 14", "Table 15", "Table 16", "Table 17", "Table 18",
     "Table 19", "Table 20"
+]*/
+
+//temporary array for items
+const tables = [
+    {name: "Classic Taco", price: 7.99}, {name:"Wacky Taco", price: 12.99}, {name: "Classic Taco", price: 7.99},
+    {name:"Wacky Taco", price: 12.99}, {name: "Classic Taco", price: 7.99}, {name:"Wacky Taco", price: 12.99},
+    {name: "Classic Taco", price: 7.99}, {name:"Wacky Taco", price: 12.99}, {name: "Classic Taco", price: 7.99},
+    {name:"Wacky Taco", price: 12.99}, {name: "Classic Taco", price: 7.99}, {name:"Wacky Taco", price: 12.99},
+    {name: "Classic Taco", price: 7.99}, {name:"Wacky Taco", price: 12.99}, {name: "Classic Taco", price: 7.99},
+    {name:"Wacky Taco", price: 12.99}, {name: "Classic Taco", price: 7.99}, {name:"Wacky Taco", price: 12.99},
+    {name: "Classic Taco", price: 7.99}, {name:"Wacky Taco", price: 12.99}
 ]
 
 //Uses custom-made modal, since there's a strange flashing that happens when changing bootstrap modals
@@ -22,12 +33,24 @@ const Modal = ({show, children}) => {
 }
 
 export default function TableModals() {
-    const [table, setTable] = useState('');
+    const [tableNum, setTableNum] = useState("-1");                 //the number of the table (1-20)
+    const [order, setOrder] = useState({name: "-1", price: "-1"});  //the name and price of menu item
+
+    const tableSets = ({target}) => {
+        if(target.dataset.index && target.dataset.name && target.dataset.price) {       //ensures nothing is undefined
+            setTableNum(() => target.dataset.index);
+            setOrder(() => ({
+                name: target.dataset.name,
+                price: target.dataset.price
+            }));
+        }
+    }
+
     const [tableShow, setTableShow] = useState(false);
-    const handleTableClick = ({target}) => {
-        const tableNum = target.value;
-        setTable(() => tableNum);
-        if(tableShow === true) {
+    const handleTableClick = ({target}) => {        //when a table button is clicked
+        tableSets({target})
+
+        if(tableShow === true) {                    //toggles table modal
             setTableShow(() => false);
         }
         else {
@@ -36,43 +59,40 @@ export default function TableModals() {
     };
 
     const [orderShow, setOrderShow] = useState(false);
-    const handleOrderClick = ({target}) => {
-        if(target.value)
-            setTable(() => target.value);
+    const handleOrderClick = ({target}) => {        //when the show order button is clicked
+        tableSets({target})
 
-        if(orderShow === true){
+        if(orderShow === true){                     //swaps between Order and Table modals
             setOrderShow(() => false);
             setTableShow(() => true);
         }
         else{
-            setOrderShow(() => true);
             setTableShow(() => false);
+            setOrderShow(() => true);
         }
     };
 
     return (
         <>
-            {tables.map(table => (
-                <button value={table} onClick={handleTableClick}>
-                    {table}
+            {tables.map((order, index) => (
+                <button data-price={order.price} data-name={order.name} data-index={index+1} onClick={handleTableClick}>
+                    Table {index+1}
                 </button>
             ))}
             <Modal show={tableShow}>
                 <button onClick={handleTableClick}>X</button>
                 <p>
-                    {table}
+                    Table {tableNum}
                     <button onClick={handleOrderClick}>Show Order</button>
                 </p>
-                <p>Classic Taco</p>
+                <p>{order.name}</p>
                 <button onClick={handleTableClick}>Complete Request</button>
             </Modal>
             <Modal show={orderShow}>
-                <button onClick={handleOrderClick}>
-                    X
-                </button>
-                <p>{table} Order</p>
+                <button onClick={handleOrderClick}>X</button>
+                <p>Table {tableNum} Order</p>
                 <p>
-                    Classic Taco, $7.99
+                    {order.name}, {order.price}
                     <button>Remove</button>
                 </p>
             </Modal>
