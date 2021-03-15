df7fb94a18c626a7fa5cb402346d5e5ffad9ad44
Ryan Cavasso <cavasso.ryan@gmail.com>
2021-03-15 18:48:01 -0500

    Displays the needs of tables when not "Occupied" or "Available"

--- src/LobbyView.js
+++ src/LobbyView.js
@@ -3,22 +3,56 @@ import Modal from './Modal';
 
 const classicTaco = {name: "Classic Taco", price: 7.99}
 const wackyTaco = {name: "Wacky Taco", price: 12.99}
-const available = {status: "Available", orders: []}
+const available = {status: "Available", orders: [], drinks: []}
 
 //temporary array for items
 const tables = [
-    {status: "Occupied", orders: [classicTaco, wackyTaco]}, {status: "Occupied", orders: [wackyTaco]},
-    available, available, available, available,  available, available, available, available, available,
-    available, available, available, available, available, available, available, available, available,
+    {status: "Refill", orders: [classicTaco, wackyTaco], drinks: ["Taco Juice", "Taco Water"]},
+    {status: "Help", orders: [wackyTaco], drinks: []}, {status: "Ready", orders: [classicTaco], drinks: []},
+    {status: "Occupied", orders: [classicTaco], drinks: []}, available, available,  available, available, available,
+    available, available, available, available, available, available, available, available, available, available,
+    available,
 ]
+
+function Needs({tableNum}) {
+    if(!tableNum)
+        return <></>
+
+    const table = tables[tableNum - 1];
+    if (table.status === "Refill") {
+        return (
+            <div>
+                Refill requested for:
+                {table.drinks.map((drink) => (
+                    <p>{drink}</p>
+                ))}
+            </div>
+        );
+    }
+    if (table.status === "Help") {
+        return(
+            <div>
+                <p>Wait staff requested.</p>
+            </div>
+        );
+    }
+    if (table.status === "Ready") {
+        return(
+            <div>
+                <p>Order ready to be delivered.</p>
+            </div>
+        )
+    } else
+        return null;
+}
 
 export default function TableModals() {
     const [tableNum, setTableNum] = useState("1");  //the number of the table (1-20)
     const [table, setTable] = useState({status: "Available", orders: []});
     const handleSetTableVals = ({target}) => {
-        if(target.dataset.index) {                  //ensures index is not undefined
+        if (target.dataset.index) {                  //ensures index is not undefined
             setTableNum(() => target.dataset.index);
-            setTable(() => tables[target.dataset.index-1]);
+            setTable(() => tables[target.dataset.index - 1]);
         }
     }
 
@@ -50,6 +84,7 @@ export default function TableModals() {
 
     const handleCompleteClick = () => {
         table.status = "Occupied";
+        //needs to actually set stuff in the data as well
         setTableShow(() => false);
     };
 
@@ -74,9 +109,7 @@ export default function TableModals() {
                     Table {tableNum}
                     <button onClick={handleOrderClick} disabled={!table.orders[0]}>Show Order</button>
                 </p>
-                {tables[tableNum-1].orders.map((order) => (
-                    <p>{order.name}</p>     //Replace with drink refill requests
-                ))}
+                <Needs tableNum={tableNum}/>
                 <button onClick={handleCompleteClick} disabled={table.status === "Available" || table.status === "Occupied"}>Complete Request</button>
             </Modal>
             <Modal show={orderShow}>
