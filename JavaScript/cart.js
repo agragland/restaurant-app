1067a9a2d408f20fd920ea83d5bcab52f2d6adf2
Rachiesqueek <Rachel.Johnson.2000@live.com>
2021-03-18 18:50:58 -0500

    Add files via upload
    -Added stuff so that others can see what I am doing
    - Need help with JavaScript Portion of menu implementation

---
+++ JavaScript/cart.js
@@ -0,0 +1,24 @@
+var array = [];
+
+function food(name, money)
+{
+    this.item = name;
+    this.price = money;
+    
+}
+
+
+function add(name, money)
+
+
+
+
+
+function show()
+{
+    for (var i=0; i < food.length; i++)
+    {
+
+        document.getElementById("Result").innerHTML = array[i];
+    }
+}
