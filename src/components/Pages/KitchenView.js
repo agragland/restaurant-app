eea9cab4f04deb4726c4163d654971346c91f6e9
Ryan Cavasso <cavasso.ryan@gmail.com>
2021-03-25 17:40:01 -0500

    Added more functionality to kitchen view, actually displays order now

--- src/components/Pages/KitchenView.js
+++ src/components/Pages/KitchenView.js
@@ -1,19 +1,21 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './KitchenView.css'
 
 const globalOrderQueue = [
-    {name: "Classic Taco", price: 7.99, table: 17},
-    {name: "Wacky Taco", price: 12.99, table: 1},
-    {name: "Wacky Taco", price: 12.99, table: 15},
-    {name: "Wacky Taco", price: 12.99, table: 16}
+    {comments: ["No shell", "Extra shell"], table: 17, status: "Created", items: [{name: "Classic Taco", price: 7.99, done: false}, {name: "Wacky Taco", price: 12.99, done: false}]},
+    {comments: ["", "help :)"], table: 1, status: "Created", items: [{name: "Classic Taco", price: 7.99, done: false}, {name: "Wacky Taco", price: 12.99, done: false}]},
+    {comments: ["Make it cheaper"], table: 10, status: "Created", items: [{name: "Wacky Taco", price: 12.99, done: false}]},
+    {comments: ["Classic."], table: 15, status: "Created", items: [{name: "Classic Taco", price: 7.99, done: false}]}
 ]
+const globalActive = [];
 
 export default function KitchenView (){
     const [orderQueue, setOrderQueue] = useState(globalOrderQueue);
-    const [actives, setActives] = useState([]);
+    const [actives, setActives] = useState(globalActive);
 
     const clickToActive = ({target}) => {
         const index = target.value
+        orderQueue[index].status = "Cooking"
         setActives((prev) => prev.concat(orderQueue[index]))
         let temp = [...orderQueue]
         temp.splice(index, 1)
@@ -22,9 +24,14 @@ export default function KitchenView (){
 
     const clickToReady = ({target}) => {
         const index = target.value
+        actives[index].status = "Ready"
         let temp = [...actives]
         temp.splice(index, 1)
         setActives(() => temp)
+    }
+
+    const clickItem = ({target}) => {
+        let isDone = actives[target.dataset.activesindex].items[target.dataset.itemindex].done
     }
 
     return (
@@ -40,10 +47,21 @@ export default function KitchenView (){
             </section>
             <section className="active-orders">
                 <h1>Active</h1>
-                {actives.map((order, index) => (
-                    <p>
-                        <button value={index} className="queue-button" onClick={clickToReady}>Table {order.table}</button>
-                    </p>
+                {actives.map((order, activesIndex) => (
+                    <section className="order">
+                        <h3 className="order-table">Table {order.table}</h3>
+                        {order.items.map((item, itemIndex) => (
+                            <div className="item">
+                                <p className="item-name">
+                                    <button onClick={clickItem} data-activesindex={activesIndex} data-itemindex={itemIndex} className={item.done}>&nbsp;</button>
+                                    {item.name}
+                                </p>
+                                <p className="item-comments">&emsp;&emsp;-{actives[activesIndex].comments[itemIndex]}</p>
+                                <br/>
+                            </div>
+                        ))}
+                        <button value={activesIndex} className="ready-button" onClick={clickToReady}>Ready</button>
+                    </section>
                 ))}
             </section>
         </div>
