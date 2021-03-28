1707403abbbd515f160ee7a0745506c5aa73aaa4
kaw0394 <kaw0394@unt.edu>
2021-03-27 23:29:02 -0500

    text input in add are set, still need to include the category selections

--- src/components/Navbar/StaffMenu.js
+++ src/components/Navbar/StaffMenu.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react';
+import AsyncCreatableSelect from 'react-select/async-creatable';
+import makeAnimated from 'react-select/animated'
 import "./staffMenu.css"
 
 //makes modal
@@ -41,10 +43,27 @@ function AddModal({show, children}) {
     ); 
 }
 
-const item = { name: '', category: '', ingredients: '', price: '', avalibility: true, image: '' }
+//all possible categories
+const Categories = [
+    { label: 'Entrees', value: 'entree' },
+    { label: 'Appetizers', value: 'appetizer' },
+    { label: 'Sides', value: 'side' },
+    { label: 'Kids', value: 'kids' },
+    { label: 'Desserts', value: 'dessert' },
+    { label: 'Drinks', value: 'drinks' },
+];
+//boolean to assist in labeling the avalibility
+const AvalBool = [
+    {label: 'Yes', value: true}, 
+    {label: 'No', value: false},
+];
+//all possible data fields needed 
+const item = { name: '', category: '', ingredients: '', price: '', avalibility: false, image: '' }
 
 //accessing the menu is something everyone can do
 export default function AccessMenu({level}) {
+   
+
     //for the levels: 
         // 0 : customer - will get access to view only
         // 1 : Lobby - will get access to view and remove
@@ -93,14 +112,19 @@ export default function AccessMenu({level}) {
         setShowDrinks((prev) => !prev);
     }
 
-    //Menu options
+        //ACCESSIBLE MENU ^^
+    //--------------------------------------------------------------------------------------------------------------------------
+        //ADD ITEM TO MENU vv
+
+    //Menu options that are avalible to passed in level
     const [menuItem, setMenuItem] = useState(item);
     let avalibleMenu = [] 
     let unavalibleMenu = []
 
-    //populate the menu from database - VERY FIRST THING TO HAPPEN
+    //populate the menu from database - VERY FIRST THING TO HAPPEN  -- should it be useEffect??
     const popMenu = () => {
-
+        //populate all data from database for menu
+        //place database data into appropriate menu array
     }
 
     //add menu item
@@ -112,14 +136,21 @@ export default function AccessMenu({level}) {
     if (level > 1){
         canAdd = <button onClick={handleAddClick}>Add Menu Item</button>;
     }
+    //set the value of an input
+
+
     //add item to database and menu
-    const AddMenu = () => {
+    const AddMenu = ({newItem}) => {
         //add to database
 
         //if avalible is true -> add to avalible menu
 
         //else (avalible is false) -> add to unavalible menu
     }
+
+        //ADD ITEM TO MENU ^^
+    //--------------------------------------------------------------------------------------------------------------------------
+        //OTHER MENU OPTIONS vv
 
     //remove an item from avalible menu - NOTE: this removes an item from avalible array
     //and places it into the unavalible array
@@ -151,12 +182,14 @@ export default function AccessMenu({level}) {
         canDelete = <button onClick={DeleteMenu}>Delete Item</button> 
     }
 
-
+    //  OTHER MENU OPTIONS ^^
+//--------------------------------------------------------------------------------------------------
+    // ACCESSIBLE MENU vv
     return (
         <div>
             <button onClick={handleModalClick} className="menu-button">Menu</button>
             <Modal show={showModal}>
-                <button onClick={handleModalClick}>X</button> &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+                <button onClick={handleModalClick}>X</button> &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
                 {canAdd}
                 <p>
                     <button onClick={handleAppetizersClick}>Appetizers</button>
@@ -195,21 +228,47 @@ export default function AccessMenu({level}) {
                     </DropMenu>
                 </p>
             </Modal>
+
+        {/* ACCESSIBLE MENU ^^
+        --------------------------------------------------------------------------------------------------
+            ADD ITEM TO MENU vv */}    
+
             <AddModal show={showAdd}>
-                <button onClick={handleAddClick}>X</button> &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-                <form>
+                <button onClick={handleAddClick}>X</button> &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+                <form onSubmit={AddMenu({menuItem})}>
                     {/*enter name*/}
-
-                    {/*select category*/}
-
+                    <label>
+                    Name: &emsp;
+                    <input type="text" placeholder="Enter name of item here" value={menuItem.name} onChange={setValue('name')} />
+                    </label>
+                    {/*select category
+                    <label>
+                        <AsyncCreatableSelect placeholder="Select Item Category" 
+                                         cacheOptions
+                                         defaultOptions
+                                         loadOptions={promiseOptions} 
+                                         />
+                    </label> */}
                     {/*enter ingredients*/}
-
+                    <br/> 
+                    <label>
+                    Ingredients: &emsp;
+                    <textarea placeholder="List all cooking ingredients here" value={menuItem.ingredients} onChange={setValue('ingredients')} />
+                    </label>
                     {/*enter price*/}
-
+                    <br/>
+                    <label>
+                    Price: &emsp;
+                    <input type="text" placeholder="Enter price of item here" value={menuItem.price} onChange={setValue('name')} />
+                    </label>
                     {/*select if avaliable*/}
-
+                    <label>
+                        Avaliblility:
+                        {/*<AsyncCreatableSelect placeholder="Is this Item Avalible?" options={AvalBool} onChange={setValue('avalibility')} />
+                    */}</label>
                     {/*collect image*/}
-
+                    <br/>
+                    <input type="submit" value="Create Menu Item" />
                 </form>
             </AddModal>
 
