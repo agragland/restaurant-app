e7dcaff79f5d05e533ad50ee837bdb28763f9555
Ryan Cavasso <cavasso.ryan@gmail.com>
2021-04-01 15:57:41 -0500

    Kitchen is fully hooked up to the database, can pull all orders and update statuses of orders. Had to scrap the working/done button, it might be able to work, but it isn't in the SRS and so may not be worth the effort.

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -9,6 +9,9 @@ export const getAllItems = () => api.get(`/items`)
 export const getItemById = id => api.get(`/item/${id}`)
 export const insertCustomer = payload => api.post(`/customer`, payload)
 export const createOrder = payload => api.post(`/order`,payload)
+export const getAllOrders = () => api.get(`/orders`)
+export const updateOrder = (id, payload) => api.put(`/order/${id}`, payload)
+export const deleteOrder = id => api.delete(`/order/${id}`)
 //export const updateMovieById = (id, payload) => api.put(`/movie/${id}`, payload)
 //export const deleteMovieById = id => api.delete(`/movie/${id}`)
 //export const getMovieById = id => api.get(`/movie/${id}`)
@@ -18,7 +21,10 @@ const apis = {
     getAllItems,
     insertCustomer,
     getItemById,
-    createOrder
+    createOrder,
+    getAllOrders,
+    updateOrder,
+    deleteOrder
 }
 
 export default apis
--- client/src/components/pages/KitchenView.js
+++ client/src/components/pages/KitchenView.js
@@ -1,23 +1,69 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './KitchenView.css'
-
-const globalOrderQueue = [
-    {comments: ["No shell", "Extra shell"], table: 17, status: "Created", items: [{name: "Classic Taco", price: 7.99}, {name: "Wacky Taco", price: 12.99}]},
-    {comments: ["", "help :)"], table: 1, status: "Created", items: [{name: "Classic Taco", price: 7.99}, {name: "Wacky Taco", price: 12.99}]},
-    {comments: ["Make it cheaper"], table: 10, status: "Created", items: [{name: "Wacky Taco", price: 12.99}]},
-    {comments: ["Classic."], table: 15, status: "Created", items: [{name: "Classic Taco", price: 7.99}]}
-]
-const globalActive = [];
+import api from "../../api";
 
 export default function KitchenView (){
-    const [orderQueue, setOrderQueue] = useState(globalOrderQueue);
-    const [actives, setActives] = useState(globalActive);
-    const [itemOrder, setItemOrder] = useState([["working", "working"], ["working", "working"], ["working"], ["working"]]);
+    const [orderQueue, setOrderQueue] = useState([]);       //array of orders in the inactive queue
+    const [actives, setActives] = useState([]);             //array of orders in the active list
+    const [itemWorking, setItemWorking] = useState([[]]);   //array for checking if an item is done
+
+    useEffect(() => {
+        handleGetOrders()
+        const intervalId = setInterval(() => {
+            handleGetOrders()
+        }, 10000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    //gets orders from the db
+    const handleGetOrders = async () => {
+        await api.getAllOrders().then(orders => {
+            const curr_orders = orders.data.data
+            console.log(curr_orders)
+            let tempActives = []
+            let tempQueue = []
+            let tempWorking = []
+            let tempItemWorking = []
+
+            curr_orders.map((order) => {
+                if (order.status === "Created") {
+                    tempQueue = [...tempQueue, order]           //adds orders to the inactive queue
+                }
+                else if (order.status === "Cooking") {
+                    tempActives = [...tempActives, order]       //adds orders to active list
+                    order.order_items.map(() => {
+                        tempWorking = [...tempWorking, "working"]   //adds "working" to itemWorking, equal to the number of items in each order
+                    })
+                    tempItemWorking = [...tempItemWorking, tempWorking]
+                    tempWorking = []
+                }
+
+            })
+
+            //set states to temps
+            setActives(tempActives)
+            setOrderQueue(tempQueue)
+            setItemWorking(tempItemWorking)
+        })
+    }
+
+    const handleUpdateOrder = async (payload) => {
+        await api.updateOrder(payload._id, payload)
+    }
 
     const clickToActive = ({target}) => {
+        //adds order to active
         const index = target.value
         orderQueue[index].status = "Cooking"
         setActives((prev) => prev.concat(orderQueue[index]))
+
+        //updates database
+        handleUpdateOrder(orderQueue[index])
+
+        //removes order from inactive
         let temp = [...orderQueue]
         temp.splice(index, 1)
         setOrderQueue(() => temp)
@@ -27,26 +73,42 @@ export default function KitchenView (){
         const index = target.value
         actives[index].status = "Ready"
 
+        //updates database
+        handleUpdateOrder(actives[index])
+
+        //removes order from active
         let temp = [...actives]
         temp.splice(index, 1)
         setActives(() => temp)
 
-        let tempWorking = [...itemOrder]
+        //removes corresponding itemWorking array
+        let tempWorking = [...itemWorking]
         tempWorking.splice(index, 1)
-        setItemOrder(() => tempWorking)
+        setItemWorking(() => tempWorking)
     }
 
     const clickItem = ({target}) => {
-        let tempArr = [...itemOrder]
+        let tempArr = [...itemWorking]
         const activesIndex = target.dataset.activesindex
         const itemIndex = target.dataset.itemindex
 
+        //toggles between working and done
         if(tempArr[activesIndex][itemIndex] === "working")
             tempArr[activesIndex][itemIndex] = "done"
         else
             tempArr[activesIndex][itemIndex] = "working"
-        setItemOrder(() => tempArr);
+        setItemWorking(() => tempArr);
     }
+
+    const giveItemWorking = (activesIndex, itemIndex) => {
+        //if itemWorking isn't empty, give the working/done button
+        if(itemWorking)
+            if(itemWorking[activesIndex])
+                if(itemWorking[activesIndex][itemIndex])
+                    return(<button onClick={clickItem} data-activesindex={activesIndex} data-itemindex={itemIndex} className={itemWorking[activesIndex][itemIndex]}>&nbsp;</button>)
+    }
+
+
 
     return (
         <div className='kitchen'>
@@ -64,10 +126,11 @@ export default function KitchenView (){
                 {actives.map((order, activesIndex) => (
                     <section className="order">
                         <h3 className="order-table">Table {order.table}</h3>
-                        {order.items.map((item, itemIndex) => (
+                        {order.order_items.map((item, itemIndex) => (
                             <div className="item">
                                 <p className="item-name">
-                                    <button onClick={clickItem} data-activesindex={activesIndex} data-itemindex={itemIndex} className={itemOrder[activesIndex][itemIndex]}>&nbsp;</button>
+                                    {/*giveItemWorking(activesIndex, itemIndex)*/}
+                                    &emsp;&nbsp;
                                     {item.name}
                                 </p>
                                 <p className="item-comments">&emsp;&emsp;-{actives[activesIndex].comments[itemIndex]}</p>
