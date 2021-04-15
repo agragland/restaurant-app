5b8eeef1b02498c69fba6082042d1fd3b7ca1c8f
Karishad <kieronyin@gmail.com>
2021-04-15 15:57:28 -0500

    Added tictactaco games in /src/games/tictactaco

---
+++ client/src/games/tictactaco/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Game from './components/game';
+
+const App = () => <Game />;
+
+export default App;
---
+++ client/src/games/tictactaco/components/board.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Square from './square';
+
+const style = {
+        border: '6px solid darkblue',
+        borderRadius: '12px',
+        width: '550px',
+        height: '550px',
+        margin: '0 auto',
+        display: 'grid',
+        gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)'
+};
+
+const Board = ({ squares, onClick }) => (
+    <div style={style}>
+        {squares.map((square, i) => (
+            <Square key={i} value={square} onClick={() => onClick(i)} />
+        ))}
+    </div>
+)
+
+    export default Board;
